b78640dbb8d6e34648ec2ce2f37715909fa2ff12
Nabil Azzuhdi <101844464+NotApplicable-NA@users.noreply.github.com>
2024-05-01 22:58:31 +0700

    Add files via upload

---
+++ components/connect.php
@@ -0,0 +1,9 @@
+<?php
+session_start();
+$db_name = 'mysql:host=localhost;dbname=shop_db';
+$user_name = 'root';
+$user_password = '';
+
+$conn = new PDO($db_name, $user_name, $user_password);
+
+?>
